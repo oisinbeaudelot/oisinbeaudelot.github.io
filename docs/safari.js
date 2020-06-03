6c1bb68349cb3ccbd75ebb8a239074dc3cee77ac
oisinbeaudelot <65941588+oisinbeaudelot@users.noreply.github.com>
2020-06-03 15:35:06 +0100

    fix safari shrink

---
+++ docs/safari.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName('h1')[0].innerHTML = document.querySelector('meta[name=viewport][content*="shrink-to-fit=no"]') ? 'shrink-to-fit=no' : 'Default; shrink-to-fit not specified' 
